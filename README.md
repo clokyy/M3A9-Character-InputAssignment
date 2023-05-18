fc78b52ac905ed6567cc209cc3f74783b5e7026f
logancw <21189821-logancw@users.noreply.replit.com>
2023-05-18 17:48:06 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+ Assignment  Goal: This program was designed to teach you how to instantiate characters. It will also show me that you are able to introduce characters, strings, and white space.  
+
+Project Description: Write a Python program to print a string of your choosing into a specific format (see the output sample).  I would like the string to be the lyrics of song related to our faith, (even though the sample does not).  
+
+Comment Requirements:   
+
+Program name: (The name you gave the program)  
+
+Student name: (Your name)  
+
+Date: (The date you completed the program)  
+
+Program Description:  
+
+*These comments are the basic expectation I have. You should include comments throughout the program.   Sample output    If you can think of another symbol that represents our faith you can shape your lyrics into that shape as well.
+
+ 
+
---
+++ main.py
@@ -0,0 +1,39 @@
+#                     M3A9 CHARACTER INPUT ASSIGNMENT
+#                            LOGAN .....
+#                       COMPLETION DATE: 3/7/2023
+
+
+#             DESCRIPTION: These are the lyrics to my hymn 
+
+#Lyrics to Hymn 
+print("You who dwell in the shelter of the Lord, Who abide in His shadow for")
+print("life Say to the Lord, My refuge, my rock in whom I trust! And He will")
+print("will raise you up on                      eagles' wings Bear you on ")
+print("the breath of dawn M                      ake you to shine like the")
+print("sun And hold you  in                      the palm of His hand  The")
+print("snare of the  fowler                      will never capture you And ")
+print("famine will bring yo                      u no fear Under his  wings")
+print("your refuge,                                       His  faithfulness")
+print("your shield,                                       And he will raise")
+print("you up on e                                        agles' wings Bear")
+print("you on the                                         breath of dawn")
+print("Make you s,                                        hine like the ")
+print("And hold y,                                        ou in the palm")
+print("of His hand You need                      not  fear the terror of the ")
+print("night Nor the arrow                       that flies by day Though th ")
+print("ousands fall about y                      ou, near you it shall not n ")
+print("ot come And he will                       raise you up on eagles' wi")
+print("ngs Bear you on the                       Breath of dawn Make you shi ")
+print("ne like the sun And                       hold you in the palm of His ")
+print("hand For His angels                       He's given command To guar ")
+print("d you in all of you                       r ways Upon their hands th ")
+print("ey will bear you up                       Lest you dash your foot aga ")
+print("inst a stone And he                       will raise you up on eagle  ")
+print("s' wings Bear you o                       on the breath of dawn   ")
+print("Make you to shine like the sun And hold you in the palm of His hand ")
+print("And hold you in the palm of His hand.")
+
+print("G Munch".upper())
+
+album = "Flip your wig"
+print(album[10:])
